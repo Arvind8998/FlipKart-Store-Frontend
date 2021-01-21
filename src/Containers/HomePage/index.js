83c64bae207a1c28b851dd187e90e321e844dce6
arvind8998 <asangwan8998@gmail.com>
2021-01-21 22:25:18 +0530

    Added categories addition functionality

---
+++ src/Containers/HomePage/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../Components/Header'
+import MenuHeader from '../../Components/MenuHeader'
+import Layout from '../../Components/Layout'
+
+function HomePage() {
+    return (
+        <Layout>
+            HomePage
+        </Layout>
+    )
+}
+
+export default HomePage
